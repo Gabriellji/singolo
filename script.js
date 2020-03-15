44bc65084132fd96ce6c425c7108fdc805484406
Gabriellji <gabriellji@yahoo.com>
2020-03-15 18:49:08 +0300

    feat: creat 'change image position' function

--- script.js
+++ script.js
@@ -1,110 +1,89 @@
 
-// active menu
+// Active menu
 
-const MENU = document.getElementById('menu');
-const anchors = document.querySelectorAll('a[href*="#"]');
+        const MENU = document.getElementById('menu');
+        const anchors = document.querySelectorAll('a[href^="#"');
 
-MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
-    event.target.classList.add('active');
-});
-
-// scroll
-
-document.querySelectorAll('a[href^="#"').forEach(link => {
-
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        let href = this.getAttribute('href').substring(1);
-        const scrollTarget = document.getElementById(href);
-        const topOffset = document.querySelector('.header').offsetHeight;
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
-
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth',
-            block: 'start'
+        MENU.addEventListener('click', (event) => {
+            MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+            event.target.classList.add('active');
         });
-    });
-});
 
-// slider
+// Scroll
 
-let items = document.querySelectorAll('.item');
-let currentItem = 0;
-let isEnabled = true;
+            anchors.forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                let href = this.getAttribute('href').substring(1);
+                const scrollTarget = document.getElementById(href);
+                const topOffset = document.querySelector('.header').offsetHeight;
+                const elementPosition = scrollTarget.getBoundingClientRect().top;
+                const offsetPosition = elementPosition - topOffset;
 
-function changeCurrentItem(n) {
-    currentItem = ( n  + items.length ) % items.length;
-}
+                window.scrollBy({
+                    top: offsetPosition,
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        });
 
-function hideItem(direction) {
-    isEnabled = false;
-    items[currentItem].classList.add(direction);
-    items[currentItem].addEventListener('animationend', function() {
-        this.classList.remove('activated', direction);
-    });
-}
+// Slider
 
-function showItem(direction) {
-    items[currentItem].classList.add('next', direction);
-    items[currentItem].addEventListener('animationend', function() {
-        this.classList.remove('next', direction);
-        this.classList.add('activated');
-        isEnabled = true;
+        let items = document.querySelectorAll('.item');
+        let currentItem = 0;
+        let isEnabled = true;
 
-    });
-}
-
-
-function previousItem(n) {
-    hideItem('to-right');
-    changeCurrentItem(n - 1);
-    showItem('from-left');
-}
-
-function nextItem(n) {
-    hideItem('to-left');
-    changeCurrentItem(n + 1);
-    showItem('from-right');
-}
-
-document.querySelector('.control.left').addEventListener('click', function() {
-     if (isEnabled) {
-         previousItem(currentItem);
-     }
-    
-    });
-
-    document.querySelector('.control.right').addEventListener('click', function() {
-        if (isEnabled) {
-            nextItem(currentItem);
+        function changeCurrentItem(n) {
+            currentItem = ( n  + items.length ) % items.length;
         }
-       
-       });
 
-// turn off phone
+        function hideItem(direction) {
+            isEnabled = false;
+            items[currentItem].classList.add(direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('activated', direction);
+            });
+        }
 
-       let phone = document.querySelector('.transperent');
-       let blackScreen = document.querySelector('.iphone-black');
+        function showItem(direction) {
+            items[currentItem].classList.add('next', direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('next', direction);
+                this.classList.add('activated');
+                isEnabled = true;
+            });
+        }
 
-       phone.addEventListener('click', function(){
-           blackScreen.classList.toggle('visible');
-       });
+        function previousItem(n) {
+            hideItem('to-right');
+            changeCurrentItem(n - 1);
+            showItem('from-left');
+          
+        }
 
-       let phone2 = document.querySelector('.transperent-2');
-       let blackScreen2 = document.querySelector('.iphone-black-2');
+        function nextItem(n) {
+            hideItem('to-left');
+            changeCurrentItem(n + 1);
+            showItem('from-right');
+           
+        }
 
-       phone2.addEventListener('click', function(){
-           blackScreen2.classList.toggle('visible');
-       });
+        document.querySelector('.control.left').addEventListener('click', function() {
+            if (isEnabled) {
+                previousItem(currentItem);
+            } 
+            });
 
+            document.querySelector('.control.right').addEventListener('click', function() {
+                if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            });
 
-   // swiper    
-       
+// Swiper    
 
-   const swipedetect = (el) => {
+    const swipedetect = (el) => {
 
     let surface = el;
     let startX = 0;
@@ -112,8 +91,6 @@ document.querySelector('.control.left').addEventListener('click', function() {
     let distX = 0;
     let distY = 0;
     let dist = 0;
-
-
     
     let startTime = 0;
     let elapsedTime = 0;
@@ -121,7 +98,6 @@ document.querySelector('.control.left').addEventListener('click', function() {
     let threshold = 150;
     let restraint = 100;
     let allowedTime = 300;
-
 
     surface.addEventListener('mousedown', function(e){
         startX = e.pageX;
@@ -146,8 +122,8 @@ document.querySelector('.control.left').addEventListener('click', function() {
 					}
 				}
 			}
-		}
-     
+        }  
+         
         e.preventDefault();
     });
 
@@ -192,11 +168,53 @@ document.querySelector('.control.left').addEventListener('click', function() {
 					}
 				}
 			}
-		}
-     
+        }
+        
         e.preventDefault();
     });
 
    }
    let el = document.querySelector('.inner-slider');
    swipedetect(el);
+
+// Turn off phone
+
+       let verticalTransperentDiv = document.querySelector('.vertical-transperent');
+       let verticalblackScreen = document.querySelector('.iphone-black');
+       let horizontalTransperentDiv = document.querySelector('.horizontal-transperent');
+       let horizontalblackScreen = document.querySelector('.iphone-black-2');
+
+       verticalTransperentDiv.addEventListener('click', () => {
+        verticalblackScreen.classList.toggle('visible');
+       });
+
+       horizontalTransperentDiv.addEventListener('click', () => {
+        horizontalblackScreen.classList.toggle('visible');
+       });
+
+
+// Portfolio
+
+
+        let portfolioTags = document.querySelector('.portfolio__tags');
+        let tags = document.querySelectorAll('.portfolio__tags .tag');
+        let imageContainer = document.querySelector('.portfolio-images');
+        let images = document.querySelectorAll('.portfolio-images img');
+
+        portfolioTags.addEventListener('click', (e) => {
+            tags.forEach(tag => tag.classList.remove('selected'));
+            e.target.classList.add('selected');
+            images.forEach(img => img.style.order = Math.floor((Math.random()*images.length)));
+        });
+
+        imageContainer.addEventListener('click', (e) => {
+            images.forEach(img => img.classList.remove('bordered'));
+            e.target.classList.add('bordered');
+        });
+
+// let sliderItems = document.querySelector('.item');
+
+
+
+
+
